10ae4e6545782837dbf46a54e4a1479abb6b6236
seungmin-dev <seungmin.dev@gmail.com>
2023-05-11 23:13:56 +0900

    유튜브 아이프레임 재생제어

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,17 @@
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import Script from "next/script";
+import WavyText from "../components/Wavytext";
 
 const Home: NextPage = ({ response }) => {
+  const [playing, setPlaying] = useState(true);
   const image = useRef<HTMLImageElement | null>(null);
   const lpImage = useRef<HTMLImageElement | null>(null);
   const title = useRef<HTMLHeadingElement | null>(null);
   const description = useRef<HTMLParagraphElement | null>(null);
+  let player: any;
 
   const onClickList = (item) => {
     image.current!.src = item.snippet.thumbnails.high.url;
@@ -19,64 +23,183 @@ const Home: NextPage = ({ response }) => {
   useEffect(() => {
     image.current!.src = response.items[0].snippet.thumbnails.high.url;
     lpImage.current!.src = response.items[0].snippet.thumbnails.medium.url;
+    title.current!.textContent = response.items[0].snippet.title;
+    description.current!.textContent = response.items[0].snippet.description;
   }, []);
+
+  const onYouTubePlayerAPIReady = () => {
+    window.YT.ready(function () {
+      player = new YT.Player("video", {
+        height: "0",
+        width: "0",
+        videoId: "jfKfPfyJRdk",
+        // videoId: response.items[0].id.videoId,
+        playerVars: { autoplay: 1, controls: 0, disablekb: 1 },
+        events: {
+          onReady: onPlayerReady,
+        },
+      });
+    });
+  };
+  var onPlayerReady = (e) => {
+    e.target.playVideo();
+  };
+  const playVideo = () => {
+    let player = document.querySelector("#video");
+    player.contentWindow.postMessage(
+      '{"event":"command","func":"playVideo","args":""}',
+      "https://www.youtube.com"
+    );
+    setPlaying((prev) => !prev);
+  };
+  const stopVideo = () => {
+    let player = document.querySelector("#video");
+    player.contentWindow.postMessage(
+      '{"event":"command","func":"pauseVideo","args":""}',
+      "https://www.youtube.com"
+    );
+    setPlaying((prev) => !prev);
+  };
+
   return (
-    <div className="w-full h-screen relative overflow-hidden pt-32 pl-52">
-      <div className="w-full h-full flex">
-        <div className="w-4/12 h-4/6 overflow-y-scroll bg-white p-8 rounded-2xl">
-          {response.items.map((item: any) => (
-            <div
-              key={item.id.videoId}
-              onClick={() => onClickList(item)}
-              className="mb-3"
-            >
-              <h5 className="text-gray-950">{item.snippet.title}</h5>
-              {/* <p className="text-gray-600">{item.snippet.description}</p> */}
-              <input type="hidden" value={item.snippet.thumbnails.high.url} />
-            </div>
-          ))}
-        </div>
-        {/* 오른쪽 부분(개별 재생화면) */}
-        <div className="pl-20">
-          <div className="rounded-full bg-black w-[20rem] h-[20rem] pt-[4rem] pl-[4rem]">
-            <Image
-              src=""
-              alt=""
-              className="relative block w-1/2 h-1/2"
-              ref={lpImage}
-              fill
-              style={{
-                objectFit: "cover",
-                objectPosition: "center",
-                width: "12rem",
-                height: "12rem",
-                borderRadius: "100%",
-                position: "relative",
-              }}
-            />
-          </div>
-          {/* 제어판 */}
-          <div></div>
-          <div className="pt-5">
-            <h2 className="text-xl text-white" ref={title}></h2>
-            <p className="text-white opacity-70" ref={description}></p>
-          </div>
-        </div>
-      </div>
-      <Image
-        src=""
-        alt=""
-        ref={image}
-        fill
-        className="blur-md"
-        style={{
-          width: "free",
-          height: "100%",
-          objectFit: "cover",
-          objectPosition: "center",
-          zIndex: -100,
+    <div className="w-full h-screen">
+      <Script
+        src="http://www.youtube.com/iframe_api"
+        onLoad={() => {
+          onYouTubePlayerAPIReady();
         }}
       />
+      <div className="w-1/2 h-full m-auto relative pt-32 pl-52 sm:pl-0 md:pl-0 lg:pl-0 xl:pl-0">
+        <div className="w-full h-full flex flex-row sm:flex-row-reverse md:flex-row">
+          {/* 왼쪽 부분(재생목록) */}
+          <div className="w-4/12 min-w-[26rem] h-4/6 overflow-y-scroll bg-white/70 p-8 rounded-2xl">
+            {response.items.map((item: any) => (
+              <div
+                key={item.id.videoId}
+                onClick={() => onClickList(item)}
+                className="mb-3"
+              >
+                <h5 className="text-gray-950">{item.snippet.title}</h5>
+                <input type="hidden" value={item.snippet.thumbnails.high.url} />
+              </div>
+            ))}
+          </div>
+          {/* 오른쪽 부분(개별 재생화면) */}
+          <div className="pl-20">
+            <div className="w-[20rem] h-[20rem] pt-[4rem] pl-[4rem] rounded-full bg-black">
+              <Image
+                src=""
+                alt=""
+                className="relative block w-1/2 h-1/2"
+                ref={lpImage}
+                fill
+                style={{
+                  objectFit: "cover",
+                  objectPosition: "center",
+                  width: "12rem",
+                  height: "12rem",
+                  borderRadius: "100%",
+                  position: "relative",
+                }}
+              />
+            </div>
+            {/* 제어판 */}
+            <div className="w-full">
+              <div className="w-3/5 m-auto pt-3 flex space-between">
+                <button className="w-10 h-10">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-10 h-10"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M21 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953l7.108-4.062A1.125 1.125 0 0121 8.688v8.123zM11.25 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953L9.567 7.71a1.125 1.125 0 011.683.977v8.123z"
+                    />
+                  </svg>
+                </button>
+                <button onClick={playing ? stopVideo : playVideo}>
+                  {playing ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-10 h-10"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M14.25 9v6m-4.5 0V9M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-10 h-10"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                      />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z"
+                      />
+                    </svg>
+                  )}
+                </button>
+                <button>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-10 h-10"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3 8.688c0-.864.933-1.405 1.683-.977l7.108 4.062a1.125 1.125 0 010 1.953l-7.108 4.062A1.125 1.125 0 013 16.81V8.688zM12.75 8.688c0-.864.933-1.405 1.683-.977l7.108 4.062a1.125 1.125 0 010 1.953l-7.108 4.062a1.125 1.125 0 01-1.683-.977V8.688z"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <div id="video"></div>
+            </div>
+            <div className="pt-5">
+              <h2 className="text-xl text-white" ref={title}></h2>
+              <p className="text-white opacity-70" ref={description}></p>
+            </div>
+          </div>
+        </div>
+        <Image
+          src=""
+          alt=""
+          ref={image}
+          fill
+          className="blur-md"
+          style={{
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+            objectPosition: "center",
+            zIndex: -100,
+          }}
+        />
+      </div>
     </div>
   );
 };
@@ -93,7 +216,6 @@ export async function getServerSideProps() {
       }
     )
   ).json();
-
   return {
     props: { response },
   };
